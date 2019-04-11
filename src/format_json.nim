ce4f0dcc0f7256fa6a1144a7568590f1faa8a918
Alexis <alexis@cvofhope.com>
2019-04-11 13:31:33 +1000

    replace line breaks

--- src/format_json.nim
+++ src/format_json.nim
@@ -1,2 +1,10 @@
-# Hello Nim!
-echo "Hello, World!"
+import strutils, os
+
+proc main(): void =
+  let str:string = paramStr(1)
+  for line in str.split("\\n"):
+    writeLine(stdout, line)
+
+if isMainModule:
+  main()
+
