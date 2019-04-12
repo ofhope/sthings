f6cabbdada488f124035f2ddf0718fa7c6530d78
Alexis <alexis@cvofhope.com>
2019-04-12 10:20:14 +1000

    add docopt

--- src/sthings.nim
+++ src/sthings.nim
@@ -1,12 +1,32 @@
+let doc = """
+Naval Fate.
+
+Usage:
+  sthings --breaklines <sinput>
+  sthings (-c=<case> --case=<case>) <sinput>
+  sthings (-h | --help)
+  sthings --version
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+  -c=<case>     Case to transform to.
+  --breaklines  Converts encoded line endings. 
+"""
+
 import strutils, os
+import docopt
 
-proc main(): void =
-  let str:string = paramStr(1)
-  for line in str.split("\\n"):
-    writeLine(stdout, line)
+let args = docopt(doc, version = "Naval Fate 2.0")
 
+proc main(args: Table[string, Value]): void =
+  if args["--breaklines"]:
+    let str:string = $(args["<sinput>"])
+    for line in str.split("\\n"):
+      writeLine(stdout, line)
+  
 if isMainModule:
-  main()
+  main(args)
 
 # snake_case
 # camelCase
--- sthings.nimble
+++ sthings.nimble
@@ -1,7 +1,7 @@
 # Package
 
 version       = "0.1.0"
-author        = "Alexis"
+author        = "Alexis Hope"
 description   = "A command line tool for string things `sthings`"
 license       = "MIT"
 srcDir        = "src"
@@ -9,4 +9,9 @@ bin           = @["sthings"]
 
 # Dependencies
 
+# [Deps]
+# --define: release
+# --opt: size
+
+requires "docopt"
 requires "nim >= 0.18.0"
